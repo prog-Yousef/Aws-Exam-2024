c732c25a62c47bc1d5138ac78a7071bc50b803bc
unknown <you@example.com>
2024-09-18 13:32:38 +0200

    nerge manuell

--- functions/GetAllBoooking/index.js
+++ functions/GetAllBoooking/index.js
@@ -1,1 +1,39 @@
 
+const { db } = require("../../services/db")
+
+
+
+// Hämtar bokningar sorterat efter datum. Men kanske räcker med att göra en scan för att hämta allt...
+module.exports.handler = async (event) => {
+    console.log(event)
+
+    const { startDate, endDate } = JSON.parse(event.body)
+
+    try {
+        const {Items} = await db.scan({
+            TableName: "bookings",
+            // IndexName: "CheckInDateIndex",
+            // KeyConditionExpression: "checkInDate >= :startDate AND checkInDate <= :endDate",
+            // ExpressionAttributeValues: {
+            //     ":startDate": startDate,
+            //     ":endDate": endDate
+            // },
+            // ScanIndexForward: true, // sorterar efter datum i fallande ordning 
+            ProjectionExpression: "bookingId, checkInDate, checkOutDate, numberOfGuests, customerName, rooms"
+        })
+
+        
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(Items)
+        }
+
+    } catch (error) {
+        console.error(error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error get bookings", error: error.message})
+        }
+    }
+}
