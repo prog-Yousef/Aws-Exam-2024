b66c920dbeb7c2a104cfe0333bd64425c55792c8
unknown <you@example.com>
2024-09-18 13:43:58 +0200

    new merge endpoints generated

--- functions/GetAllBooking/index.js
+++ functions/GetAllBooking/index.js
@@ -1,39 +1,40 @@
 
-const { db } = require("../../services/db")
+const { db } = require("../../services/db");
 
 
 
 // Hämtar bokningar sorterat efter datum. Men kanske räcker med att göra en scan för att hämta allt...
 module.exports.handler = async (event) => {
-    console.log(event)
+    console.log(event);
 
-    const { startDate, endDate } = JSON.parse(event.body)
+    const { startDate, endDate } = JSON.parse(event.body);
 
     try {
-        const {Items} = await db.scan({
+        const { Items } = await db.scan({
             TableName: "bookings",
             // IndexName: "CheckInDateIndex",
             // KeyConditionExpression: "checkInDate >= :startDate AND checkInDate <= :endDate",
             // ExpressionAttributeValues: {
             //     ":startDate": startDate,
-            //     ":endDate": endDate
+            //     ":endDate": endDate,
             // },
             // ScanIndexForward: true, // sorterar efter datum i fallande ordning 
-            ProjectionExpression: "bookingId, checkInDate, checkOutDate, numberOfGuests, customerName, rooms"
-        })
-
-        
+            ProjectionExpression: "bookingId, checkInDate, checkOutDate, numberOfGuests, customerName, rooms",
+        });
 
         return {
             statusCode: 200,
-            body: JSON.stringify(Items)
-        }
+            body: JSON.stringify(Items),
+        };
 
     } catch (error) {
-        console.error(error)
+        console.error(error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: "Error get bookings", error: error.message})
-        }
+            body: JSON.stringify({
+                message: "Error get bookings",
+                error: error.message,
+            }),
+        };
     }
-}
+};
