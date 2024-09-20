ce2cdebb2d5507e4718f906a58c732e39d6a1fca
Kristofer_Almeros <kristofer.almeros@hotmail.com>
2024-09-20 09:44:24 +0200

    ändrat metod från put till update och gjort det dynamiskt beroende på vad som skickas in, samt lite valideringar

--- functions/UpdateBooking/index.js
+++ functions/UpdateBooking/index.js
@@ -8,9 +8,24 @@ exports.handler = async (event) => {
     try {
         const { id } = event.pathParameters
 
+        if (!event.body) {
+            return {
+                statusCode: 400,
+                body: json.stringify({ message: "Body is missing" })
+            }
+        }
+
         const { guests, rooms, checkInDate, checkOutDate } = JSON.parse(event.body)
 
-        // Hämta bokningen
+        // Om inget skickats in så avbryt
+        if (!guests && !rooms && !checkInDate && !checkOutDate) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "No fields to update" })
+            }
+        }
+
+        // Hämta bokningen för att jämföra om guests eller rooms har ändrats
         const bookingResult = await db.get({
             TableName: "bookings",
             Key: { bookingId: id }
@@ -27,11 +42,22 @@ exports.handler = async (event) => {
 
         console.log("Booking:", booking)
 
-        // Validera antal rum och antal gäster beroende på vad som skickats in, antingen rum eller gäster eller både och
         const updatedGuests = guests || booking.guests
         const updatedRooms = rooms || booking.rooms
+        const updatedCheckInDate = checkInDate || booking.checkInDate
+        const updatedCheckOutDate = checkOutDate || booking.checkOutDate
 
-        // Kör bara om guests eller rooms har ändrats. Var tvungen att göra om rooms till strängar. Körs bara on något har ändrats
+        // Kolla om något har förändrats, annars returnera
+        if (updatedGuests === booking.guests && JSON.stringify(updatedRooms) === JSON.stringify(booking.rooms) &&
+            updatedCheckInDate === booking.checkInDate && updatedCheckOutDate === booking.checkOutDate
+        ) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "Nothing has changed from your booking" })
+            }
+        }
+
+        // Kör validering bara om guests eller rooms har ändrats. Var tvungen att göra om rooms till strängar. 
         if (updatedGuests !== booking.guests || JSON.stringify(updatedRooms) !== JSON.stringify(booking.rooms)) {
             const validation = await validateGuestsAndRooms(updatedGuests, updatedRooms)
             if (!validation.valid) {
@@ -42,22 +68,49 @@ exports.handler = async (event) => {
             }
         }
         
-        // Beräka total kostnad ifall rooms har ändrats, annars behåll. Gör om till strängar för att kunna jämföra arrays och objects...
+        // Beräka total kostnad ifall rooms har ändrats, annars behåll. Gör om till strängar för att kunna jämföra arrays med objects...
         const updatedTotalCost = JSON.stringify(updatedRooms) !== JSON.stringify(booking.rooms) ? await calculateTotalPrice(updatedRooms) : booking.totalCost
-        
-        const updatedBooking = {
-            ...booking,
-            guests: guests || booking.guests,
-            rooms: rooms || booking.rooms,
-            checkInDate: checkInDate || booking.checkInDate,
-            checkOutDate: checkOutDate || booking.checkOutDate,
-            totalCost: updatedTotalCost
+
+        // UpdateExpressions beroende på vad som skickats in
+        let updateExpressions = []
+        let expressionAttributeValues = {}
+
+        if (guests) {
+            updateExpressions.push("guests = :guests")
+            expressionAttributeValues[":guests"] = guests
         }
 
-        await db.put({
+        if (rooms) {
+            updateExpressions.push("rooms = :rooms")
+            expressionAttributeValues[":rooms"] = rooms
+        }
+
+        if (checkInDate) {
+            updateExpressions.push("checkInDate = :checkInDate")
+            expressionAttributeValues[":checkInDate"] = checkInDate
+        }
+
+        if (checkOutDate) {
+            updateExpressions.push("checkOutDate = :checkOutDate")
+            expressionAttributeValues[":checkOutDate"] = checkOutDate
+        }
+
+        updateExpressions.push("totalCost = :totalCost")
+        expressionAttributeValues[":totalCost"] = updatedTotalCost
+
+        const updateExpression = "SET " + updateExpressions.join(", ")
+
+        const updateResult = await db.update({
             TableName: "bookings",
-            Item: updatedBooking
+            Key: { bookingId: id},
+            UpdateExpression: updateExpression,
+            ExpressionAttributeValues: expressionAttributeValues,
+            ReturnValues: "ALL_NEW"
         })
+
+        console.log("Update result:", updateResult)
+
+        const updatedBooking = updateResult.Attributes
 
         return {
             statusCode: 200,
